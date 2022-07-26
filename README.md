43306915285b24ea97daa28330bb3078535cf862
renzmann <32076780+renzmann@users.noreply.github.com>
2022-07-25 20:53:42 -0400

    add markdown-mode and update readme with installation instruction

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # .emacs.d
-My emacs configuration
+My emacs configuration. Want to use it? Go ahead!
+
+```sh
+cd ~ && git clone https://github.com/renzmann/.emacs.d
+```
--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit)))
+ '(package-selected-packages '(markdown-mode magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
