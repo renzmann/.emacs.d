cd4d1e1bdf11255af618686f5e88ebb85667a66f
Robb Enzmann <robbenzmann@gmail.com>
2022-07-26 13:35:03 -0400

    change string color

--- custom.el
+++ custom.el
@@ -11,6 +11,6 @@
  ;; If there is more than one, they won't work right.
  '(button ((t (:background "#333333" :foreground "light sky blue" :underline t))))
  '(font-lock-comment-face ((t (:foreground "gray45" :slant italic))))
- '(font-lock-string-face ((t (:foreground "orange2"))))
+ '(font-lock-string-face ((t (:foreground "DarkGoldenrod1"))))
  '(line-number ((t (:foreground "gray34"))))
  '(line-number-current-line ((t (:inherit line-number :foreground "gray71")))))
