0f319f5f191805043c0246ddb90a8ad12af85ac3
renzmann <32076780+renzmann@users.noreply.github.com>
2022-07-25 20:32:34 -0400

    remove lsp-mode from packages

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit lsp-mode)))
+ '(package-selected-packages '(magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
