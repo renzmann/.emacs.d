1b4cffb759b0da251317241b76f9ed60f73d17e4
Robb Enzmann <robbenzmann@gmail.com>
2022-08-03 17:56:19 +0000

    set icomplete mode always
    If on a modern emacs, use vertical mode

--- init.el
+++ init.el
@@ -122,35 +122,16 @@
 (setq tab-always-indent 'complete)
 
 ;; Enable fuzzy matching in minibuffer
-;; (setq completion-styles '(flex basic))
-;; IComplete vertical mode is still a little crazy - C-j vs. RET vs. C-M-i vs. TAB
-;; (setq icomplete-scroll t)
-;; (setq icomplete-show-matches-on-no-input t)
-(if (version< emacs-version "28.1")
-    (progn
-      (progn
-        ;; make buffer switch command do suggestions, also for find-file command
-        (require 'ido)
-        (ido-mode 1)
-        ;; show choices vertically
-        (setf (nth 2 ido-decorations) "\n")
-        ;; show any name that has the chars you typed
-        (setq ido-enable-flex-matching t)
-        ;; use current pane for newly opened file
-        (setq ido-default-file-method 'selected-window)
-        ;; use current pane for newly switched buffer
-        (setq ido-default-buffer-method 'selected-window))
-      (progn
-        ;; minibuffer enhanced completion icomplete
-        (require 'icomplete)
-        (icomplete-mode 1)
-        ;; show choices vertically
-        (setq icomplete-separator "\n")
-        (setq icomplete-hide-common-prefix nil)
-        (setq icomplete-in-buffer t)
-        (define-key icomplete-minibuffer-map (kbd "<right>") 'icomplete-forward-completions)
-        (define-key icomplete-minibuffer-map (kbd "<left>") 'icomplete-backward-completions)))
-  (fido-vertical-mode 1))
+(require 'icomplete)
+(setq completion-styles '(flex basic))
+(setq icomplete-scroll t)
+(setq icomplete-show-matches-on-no-input t)
+(setq icomplete-hide-common-prefix nil)
+(setq icomplete-in-buffer t)
+(unless (version< emacs-version "28.1")
+  (setq icomplete-vertical-mode t))
+(icomplete-mode 1)
+
 
 ;; Non-default configuration
 ;; ============================================================================
