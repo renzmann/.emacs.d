24ff12fc0bc71d85073b539a0c3693d0d12eb210
Robb Enzmann <robbenzmann@gmail.com>
2022-08-08 09:56:22 -0400

    make icomplete highlighting less extreme in most cases

--- custom.el
+++ custom.el
@@ -29,4 +29,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(font-lock-comment-face ((t (:foreground "#616e88" :slant italic))))
+ '(icomplete-selected-match ((t (:background "gray25"))))
  '(whitespace-tab ((t (:foreground "#aa9e9f")))))
