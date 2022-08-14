dadfab6d238d9056daa58fe42f26bb65583f07c1
Robb Enzmann <robbenzmann@gmail.com>
2022-08-14 14:54:52 -0400

    add semantic mode and make "mypy" safe

--- init.el
+++ init.el
@@ -9,7 +9,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(marginalia)))
+ '(package-selected-packages '(marginalia))
+ '(safe-local-variable-values '((python-check-command . "mypy"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -117,7 +118,11 @@
 
 ;; Disable asking about risky variables from .dir-locals.el
 ;; https://emacs.stackexchange.com/a/44604
-;; (advice-add 'risky-local-variable-p :override #'ignore)
+(advice-add 'risky-local-variable-p :override #'ignore)
+
+;; Enable semantic mode for more intelligent code parsing
+;; https://www.gnu.org/software/emacs/manual/html_node/semantic/Semantic-mode.html
+(semantic-mode)
 
 ;; Autocompletion
 ;; ============================================================================
