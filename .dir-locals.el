aee755c449edff3b9089337ce546fcc4ec746d3b
renzmann <32076780+renzmann@users.noreply.github.com>
2022-08-18 21:01:45 -0400

    move eglot/pyvenv to MELPA versions, add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode . ((python-shell-virtualenv-root . ".venv"))))
--- elpa/pyvenv-1.21/pyvenv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from pyvenv.el  -*- no-byte-compile: t -*-
-(define-package "pyvenv" "1.21" "Python virtual environment interface" 'nil :commit "103d2f158ef2a760741682e18741e44107c68f3f" :authors '(("Jorgen Schaefer" . "contact@jorgenschaefer.de")) :maintainer '("Jorgen Schaefer" . "contact@jorgenschaefer.de") :keywords '("python" "virtualenv" "tools") :url "http://github.com/jorgenschaefer/pyvenv")
