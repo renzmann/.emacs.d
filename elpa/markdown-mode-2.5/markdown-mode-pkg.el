774a26766df4325c586e2c33fa63a82d46551d7c
renzmann <32076780+renzmann@users.noreply.github.com>
2022-08-19 16:01:13 -0400

    add markdown mode package

---
+++ elpa/markdown-mode-2.5/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from markdown-mode.el  -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "2.5" "Major mode for Markdown-formatted text" '((emacs "25.1")) :commit "eecf2f20b097f9e6a0eaf938af967122fbec35dd" :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :keywords '("markdown" "github flavored markdown" "itex") :url "https://jblevins.org/projects/markdown-mode/")
