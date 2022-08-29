832bc632c6e3f2538896166bccba4d7ab542223e
renzmann <32076780+renzmann@users.noreply.github.com>
2022-08-29 08:00:48 -0400

    switch ef-theme config to ELPA version

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(markdown-mode eglot pyvenv marginalia))
+ '(package-selected-packages '(ef-themes markdown-mode eglot pyvenv marginalia))
  '(safe-local-variable-values
    '((python-check-command . "poetry run pyright")
      (python-shell-virtualenv-root . ".venv")
@@ -41,8 +41,6 @@
 ;; Prot's themes have been reliably legible in nearly every situation.
 ;; As of writing, his ef-themes are under active development, so I'm
 ;; tracking it manually.
-(add-to-list 'load-path "~/.emacs.d/manual-packages/ef-themes")
-(mapc #'disable-theme custom-enabled-themes)
 (require 'ef-themes)
 
 (setq ef-themes-headings ; read the manual's entry of the doc string
