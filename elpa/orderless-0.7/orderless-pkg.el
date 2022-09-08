687651a2f1daf548dc1cc2ff79bb9d227f6fae12
Robb Enzmann <robbenzmann@gmail.com>
2022-09-08 18:47:43 -0400

    add orderless

---
+++ elpa/orderless-0.7/orderless-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from orderless.el  -*- no-byte-compile: t -*-
+(define-package "orderless" "0.7" "Completion style for matching regexps in any order" '((emacs "26.1")) :commit "92008e762b30cb445a2227e458cbb9a5e1b1d4e8" :authors '(("Omar Antolín Camarena" . "omar@matem.unam.mx")) :maintainer '("Omar Antolín Camarena" . "omar@matem.unam.mx") :keywords '("extensions") :url "https://github.com/oantolin/orderless")
