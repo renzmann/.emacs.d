1ceed422e581403a5695d8cf0916c551b4390a99
Robb Enzmann <robbenzmann@gmail.com>
2022-09-12 15:01:39 -0400

    move vterm config to macOS section

--- init.el
+++ init.el
@@ -169,10 +169,6 @@
 (require 'evil)
 (evil-mode 1)
 
-;; Better terminal emulation
-(require 'vterm)
-(add-hook 'vterm-mode-hook 'turn-off-evil-mode)
-
 
 
 ;; ============================================================================
@@ -397,7 +393,13 @@
   ;; Uncomment this if we can't install Hack Nerd font
   ;; (set-face-attribute 'default nil :font "Menlo-14")
   (set-face-attribute 'default nil :font "Hack Nerd Font Mono-13")
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+
+  ;; Better terminal emulation
+  (require 'vterm)
+  (add-hook 'vterm-mode-hook 'turn-off-evil-mode)
+  (add-hook 'vterm-mode-hook (lambda () (setq-local global-hl-line-mode nil)))
+  )
 
 
 
