21728cd214742e8ae001cb101bd0b924e5f32dd0
renzmann <32076780+renzmann@users.noreply.github.com>
2022-09-13 21:05:24 -0400

    add a note on TRAMP config

--- init.el
+++ init.el
@@ -431,6 +431,15 @@
 ;; ============================================================================
 (add-to-list 'tramp-remote-path "~/.local/bin")
 (add-to-list 'tramp-remote-path "~/.conda/envs/rae/bin")
+;; TODO look into these - https://github.com/doomemacs/doomemacs/issues/3909
+;; (setq tramp-inline-compress-start-size 1000)
+;; (setq tramp-copy-size-limit 10000)
+;; (setq vc-handled-backends '(Git))
+;; (setq tramp-verbose 1)
+;; (setq tramp-default-method "scp")
+;; (setq tramp-use-ssh-controlmaster-options nil)
+;; (setq projectile--mode-line "Projectile")
+;; (setq tramp-verbose 1)
 
 
 
