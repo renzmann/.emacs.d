56eeb0062ba16f6540da209cdd5f23b9c8a38d06
Robb Enzmann <robbenzmann@gmail.com>
2022-10-12 15:25:31 -0400

    add a potential snippet for SQL-specific indentation

--- init.el
+++ init.el
@@ -331,6 +331,31 @@
 ;;                                SQL
 ;; ============================================================================
 (setq-default indent-tabs-mode nil)
+;; (setq electric-indent-inhibit t)
+
+;; https://emacs.stackexchange.com/a/16513
+(defun renz/sql-indent ()
+  (let ((last-indent (if (> (line-number-at-pos) 1)
+                         (save-excursion
+                           (previous-line)
+                           (back-to-indentation)
+                           (current-column))
+                       0)))
+    (save-excursion
+      (back-to-indentation)
+      (if (and (eq last-command this-command)
+               (> (point) (line-beginning-position)))
+          (delete-region (max (line-beginning-position) (- (point) 4)) (point))
+        (while (< (current-column) (+ 4 last-indent))
+          (insert " "))))
+    (if (< (point) (save-excursion (back-to-indentation) (point)))
+        (back-to-indentation))))
+
+
+(defun renz/sql-mode-hook ()
+  (setq indent-line-function 'renz/sql-indent))
+
+;; (add-hook 'sql-mode-hook 'renz/sql-mode-hook)
 (require 'hive2)
 (require 'ob-sql-mode)
 (add-to-list 'auto-mode-alist '("\\.hql" . sql-mode))
