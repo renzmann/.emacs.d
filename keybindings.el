6b47634372c5291756d656723734ab4e56963d35
Robb Enzmann <robbenzmann@gmail.com>
2022-10-17 14:42:07 -0400

    cleanup zap, anonymous function references, and projectile prefix

--- keybindings.el
+++ keybindings.el
@@ -24,10 +24,15 @@
 ;; Expanded defaults
 ;; ----------------------------------------
 (global-set-key (kbd "C-M-<backspace>") 'backward-kill-sexp)
-(global-set-key (kbd "C-M-z") 'zap-up-to-char)
+
+;; UNBINDS the suspend-frame keybinding, which is annoying on GUI.
+;; Use C-x C-z if you really want to suspend the frame.
+(global-set-key (kbd "C-z") #'zap-up-to-char)
 
 ;; A better version of `dabbrev'
 ;; https://www.masteringemacs.org/article/text-expansion-hippie-expand
+;; I tend to prefer this on its own key, since I like a lot of the
+;; default dabbrev behavior.
 ;; (global-set-key [remap dabbrev-expand] 'hippie-expand)
 
 ;; Better buffer list for C-x C-b
@@ -42,23 +47,23 @@
 ;; C-c <letter> bindings
 ;; ----------------------------------------
 (global-set-key (kbd "C-c a") #'org-agenda)
-;; (global-set-key (kbd "C-c b") ')
+;; (global-set-key (kbd "C-c b") #')
 (global-set-key (kbd "C-c c") #'org-capture)
 (global-set-key (kbd "C-c d") #'renz/find-tag)
-;; (global-set-key (kbd "C-c e") ')
+;; (global-set-key (kbd "C-c e") #')
 (global-set-key (kbd "C-c f") #'hippie-expand)
-;; (global-set-key (kbd "C-c g") ')
-;; (global-set-key (kbd "C-c h") ')
+;; (global-set-key (kbd "C-c g") #')
+;; (global-set-key (kbd "C-c h") #')
 (require 'change-inner)
 (global-set-key (kbd "C-c i") #'change-inner)
 (global-set-key (kbd "C-c j") #'imenu)  ; matches major modes that use C-c C-j
-;; (global-set-key (kbd "C-c k") ')
+;; (global-set-key (kbd "C-c k") #')
 (global-set-key (kbd "C-c l") #'eglot)
 (global-set-key (kbd "C-c m") #'ef-themes-toggle)
-;; (global-set-key (kbd "C-c n") ')
+;; (global-set-key (kbd "C-c n") #')
 (global-set-key (kbd "C-c o") #'change-outer)
-;; (global-set-key (kbd "C-c p") ')
-;; (global-set-key (kbd "C-c q") ')
+(global-set-key (kbd "C-c p") #'projectile-command-map)
+;; (global-set-key (kbd "C-c q") #')
 (global-set-key (kbd "C-c r") #'renz/recentf-find-file)
 (global-set-key (kbd "C-c s s") #'renz/jump-configuration)
 (global-set-key (kbd "C-c s i") #'renz/jump-init)
@@ -66,34 +71,34 @@
 (global-set-key (kbd "C-c t d") #'org-babel-detangle)
 (global-set-key (kbd "C-c t o") #'org-babel-tangle-jump-to-org)
 (global-set-key (kbd "C-c t s") #'renz/org-babel-tangle-jump-to-src)
-;; (global-set-key (kbd "C-c u") ')
-;; (global-set-key (kbd "C-c v") ')
+;; (global-set-key (kbd "C-c u") #')
+;; (global-set-key (kbd "C-c v") #')
 (global-set-key (kbd "C-c w") #'renz/org-kill-src-block)
-;; (global-set-key (kbd "C-c x") ')
-;; (global-set-key (kbd "C-c y") ')
-;; (global-set-key (kbd "C-c z") ')
+;; (global-set-key (kbd "C-c x") #')
+;; (global-set-key (kbd "C-c y") #')
+;; (global-set-key (kbd "C-c z") #')
 
 ;; ----------------------------------------
 ;; F5 - F9
 ;; ----------------------------------------
-(global-set-key (kbd "<f5>") 'compile)
-(global-set-key (kbd "M-<f5>") 'recompile)
-;; (global-set-key (kbd "<f6>") ')
-;; (global-set-key (kbd "M-<f6>") ')
-;; (global-set-key (kbd "<f7>") ')
-;; (global-set-key (kbd "M-<f7>") ')
-;; (global-set-key (kbd "<f8>") ')
-;; (global-set-key (kbd "M-<f8>") ')
-(global-set-key (kbd "<f9>") 'vterm)
-(global-set-key (kbd "M-<f9>") 'eshell)
-(global-set-key (kbd "S-<f9>") 'ansi-term)
-(global-set-key (kbd "s-<f9>") 'shell)
+(global-set-key (kbd "<f5>") #'compile)
+(global-set-key (kbd "M-<f5>") #'recompile)
+;; (global-set-key (kbd "<f6>") #')
+;; (global-set-key (kbd "M-<f6>") #')
+;; (global-set-key (kbd "<f7>") #')
+;; (global-set-key (kbd "M-<f7>") #')
+;; (global-set-key (kbd "<f8>") #')
+;; (global-set-key (kbd "M-<f8>") #')
+(global-set-key (kbd "<f9>") #'vterm)
+(global-set-key (kbd "M-<f9>") #'eshell)
+(global-set-key (kbd "S-<f9>") #'ansi-term)
+(global-set-key (kbd "s-<f9>") #'shell)
 
 ;; ----------------------------------------
 ;; Nonstandard bindings
 ;; ----------------------------------------
 (require 'expand-region)
 (global-set-key (kbd "C-=") #'er/expand-region)
-(if (eq system-type 'darwin)
-    (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
+(when (eq system-type 'darwin)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map))
