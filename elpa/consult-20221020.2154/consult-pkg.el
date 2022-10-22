0248e6d0942f241d3e3795dc019ce65b18dedb3c
Robb Enzmann <robbenzmann@gmail.com>
2022-10-21 22:07:37 -0400

    add consult package

---
+++ elpa/consult-20221020.2154/consult-pkg.el
@@ -0,0 +1,11 @@
+(define-package "consult" "20221020.2154" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "28.1"))
+  :commit "3226c1a01e0a2a4352b2c885b8f4eb22610d99af" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
