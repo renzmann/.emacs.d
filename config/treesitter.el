a3bcdbc57651ec0d221cb413a153bc5a43488fc4
Robb Enzmann <EnzmannR@aetna.com>
2022-10-24 10:26:10 -0400

    don't load tree-sitter eagerly on specific host

--- config/treesitter.el
+++ config/treesitter.el
@@ -1,7 +1,8 @@
 ;; ============================================================================
 ;; 			      TreeSitter
 ;; ============================================================================
-(require 'tree-sitter)
-(require 'tree-sitter-langs)
-(global-tree-sitter-mode)
-(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+(unless (string= (getenv "HOSTNAME") "xhadrevrm7p.aetna.com")
+  (require 'tree-sitter)
+  (require 'tree-sitter-langs)
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
