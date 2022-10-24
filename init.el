b1e7bfc4c6afd390dddee11a67a52440a9ee1e77
renzmann <32076780+renzmann@users.noreply.github.com>
2022-10-23 23:25:58 -0400

    add emojify sources

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  '(eldoc-echo-area-use-multiline-p nil)
  '(evil-undo-system 'undo-redo)
  '(package-selected-packages
-   '(corfu-terminal corfu esup consult doom-themes org-roam ob-async ob-ipython sqlup-mode minimap sql-indent ripgrep projectile blacken lsp-mode sqlformat pythonic f s reformatter change-inner expand-region vterm evil magit vertico tree-sitter-langs tree-sitter orderless ob-sql-mode yaml-mode exec-path-from-shell vimrc-mode csv-mode haskell-mode julia-mode lua-mode go-mode scala-mode rust-mode ef-themes markdown-mode eglot marginalia)))
+   '(emojify corfu-terminal corfu esup consult doom-themes org-roam ob-async ob-ipython sqlup-mode minimap sql-indent ripgrep projectile blacken lsp-mode sqlformat pythonic f s reformatter change-inner expand-region vterm evil magit vertico tree-sitter-langs tree-sitter orderless ob-sql-mode yaml-mode exec-path-from-shell vimrc-mode csv-mode haskell-mode julia-mode lua-mode go-mode scala-mode rust-mode ef-themes markdown-mode eglot marginalia)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
