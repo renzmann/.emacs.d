9006222f3b0be79743f26f6aef9659086d1b6e47
Robb Enzmann <robbenzmann@gmail.com>
2022-10-24 21:57:07 -0400

    more minimap config and add pyvenv to selected-packages (again)

--- config/minimap.el
+++ config/minimap.el
@@ -2,7 +2,4 @@
 ;;                            Minimap Display
 ;; ============================================================================
 (when (display-graphic-p)
-  (minimap-mode t)
-  (setq minimap-minimum-width 20)
-  (setq minimap-width-fraction 0.1)
-  (setq minimap-window-location 'right))
+  (minimap-mode t))
