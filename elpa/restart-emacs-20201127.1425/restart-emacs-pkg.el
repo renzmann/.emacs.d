b58eb91b552d778465fc72e6d1ff941dac9a6722
renzmann <32076780+renzmann@users.noreply.github.com>
2022-10-26 21:20:05 -0400

    add restart-emacs source

---
+++ elpa/restart-emacs-20201127.1425/restart-emacs-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from restart-emacs.el  -*- no-byte-compile: t -*-
+(define-package "restart-emacs" "20201127.1425" "Restart emacs from within emacs" 'nil :commit "d0fca7fba014b2d0d4dedcb9744a1e73cd9a6409" :authors '(("Iqbal Ansari" . "iqbalansari02@yahoo.com")) :maintainer '("Iqbal Ansari" . "iqbalansari02@yahoo.com") :keywords '("convenience") :url "https://github.com/iqbalansari/restart-emacs")
