2ffb2a5f91093545a6a7c7941f20aaf6c3d8c3b4
renzmann <32076780+renzmann@users.noreply.github.com>
2022-10-30 22:40:03 -0400

    add ox-hugo, diff-hl, and ox-hugo config

--- init.el
+++ init.el
@@ -18,8 +18,9 @@
  '(minimap-width-fraction 0.0)
  '(minimap-window-location 'right)
  '(org-agenda-files '("~/.emacs.d/org/work.org"))
+ '(org-hugo-front-matter-format "yaml")
  '(package-selected-packages
-   '(minimap tramp restart-emacs gnuplot corfu-terminal corfu esup consult org-roam ob-async sqlup-mode sql-indent ripgrep blacken lsp-mode sqlformat pythonic f s reformatter change-inner expand-region vterm evil magit vertico tree-sitter-langs tree-sitter orderless ob-sql-mode yaml-mode exec-path-from-shell vimrc-mode csv-mode haskell-mode julia-mode lua-mode go-mode scala-mode rust-mode ef-themes markdown-mode eglot marginalia)))
+   '(diff-hl ox-hugo minimap tramp restart-emacs gnuplot corfu-terminal corfu esup consult org-roam ob-async sqlup-mode sql-indent ripgrep blacken lsp-mode sqlformat pythonic f s reformatter change-inner expand-region vterm evil magit vertico tree-sitter-langs tree-sitter orderless ob-sql-mode yaml-mode exec-path-from-shell vimrc-mode csv-mode haskell-mode julia-mode lua-mode go-mode scala-mode rust-mode ef-themes markdown-mode eglot marginalia)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
