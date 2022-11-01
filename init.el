c94b7b13dca531cddee2f902e0e89cb3fb308670
Robb Enzmann <robbenzmann@gmail.com>
2022-11-01 08:58:20 -0400

    remove minimap from selected packages

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  '(org-agenda-files '("~/.emacs.d/org/work.org"))
  '(org-hugo-front-matter-format "yaml")
  '(package-selected-packages
-   '(poly-markdown diff-hl ox-hugo minimap tramp restart-emacs gnuplot corfu-terminal corfu esup consult org-roam ob-async sqlup-mode sql-indent ripgrep blacken sqlformat pythonic f s reformatter change-inner vterm magit vertico tree-sitter-langs tree-sitter orderless ob-sql-mode yaml-mode exec-path-from-shell vimrc-mode csv-mode haskell-mode julia-mode lua-mode go-mode scala-mode rust-mode ef-themes markdown-mode eglot marginalia)))
+   '(poly-markdown diff-hl ox-hugo tramp restart-emacs gnuplot corfu-terminal corfu esup consult org-roam ob-async sqlup-mode sql-indent ripgrep blacken sqlformat pythonic f s reformatter change-inner vterm magit vertico tree-sitter-langs tree-sitter orderless ob-sql-mode yaml-mode exec-path-from-shell vimrc-mode csv-mode haskell-mode julia-mode lua-mode go-mode scala-mode rust-mode ef-themes markdown-mode eglot marginalia)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
