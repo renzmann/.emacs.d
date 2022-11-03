7c92467295ffdca9b499385b32d8ded2d2bd2e60
Robb Enzmann <robbenzmann@gmail.com>
2022-11-03 19:37:09 -0400

    make org headers large again

--- config/theme.el
+++ config/theme.el
@@ -2,21 +2,22 @@
 ;;                           Color Theme
 ;; ============================================================================
 ;; Prot's themes have been reliably legible in nearly every situation.
-;; (setq ef-themes-headings ; read the manual's entry of the doc string
-;;       '((0 . (1.9))
-;;         (1 . (1.8))
-;;         (2 . (1.7))
-;;         (3 . (1.6))
-;;         (4 . (1.5))
-;;         (5 . (1.4)) ; absence of weight means `bold'
-;;         (6 . (1.3))
-;;         (7 . (1.2))
-;;         (t . (1.1))))
+(setq ef-themes-headings ; read the manual's entry of the doc string
+      '((0 . (1.9))
+        (1 . (1.8))
+        (2 . (1.7))
+        (3 . (1.6))
+        (4 . (1.5))
+        (5 . (1.4)) ; absence of weight means `bold'
+        (6 . (1.3))
+        (7 . (1.2))
+        (t . (1.1))))
 
 (setq ef-themes-to-toggle '(ef-light ef-trio-dark))
 
 ;; TODO: load based on time of day
-(load-theme 'ef-trio-dark :no-confirm)
+;; (load-theme 'ef-trio-dark :no-confirm)
+(load-theme 'ef-cherie :no-confirm)
 
 ;; LOVE these:
 ;; Light:
