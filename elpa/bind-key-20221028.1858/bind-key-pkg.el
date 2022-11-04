e99e9338af962195b269c4fe0ccf27fa0697a72c
Robb Enzmann <robbenzmann@gmail.com>
2022-11-04 19:24:47 -0400

    add use-package sources from melpa

---
+++ elpa/bind-key-20221028.1858/bind-key-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from bind-key.el  -*- no-byte-compile: t -*-
+(define-package "bind-key" "20221028.1858" "A simple way to manage personal keybindings" 'nil :commit "4095f1bdae7989981803d25a52384383116e6db6" :authors '(("John Wiegley" . "johnw@newartisans.com")) :maintainer '("John Wiegley" . "johnw@newartisans.com") :keywords '("keys" "keybinding" "config" "dotemacs") :url "https://github.com/jwiegley/use-package")
---
+++ elpa/use-package-20221029.1857/use-package-pkg.el
@@ -0,0 +1,13 @@
+(define-package "use-package" "20221029.1857" "A configuration macro for simplifying your .emacs"
+  '((emacs "24.3")
+    (bind-key "2.4"))
+  :commit "0be480ea77655304a4e6aa9d38d9d9a251b2bd32" :authors
+  '(("John Wiegley" . "johnw@newartisans.com"))
+  :maintainer
+  '("John Wiegley" . "johnw@newartisans.com")
+  :keywords
+  '("dotemacs" "startup" "speed" "config" "package")
+  :url "https://github.com/jwiegley/use-package")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
