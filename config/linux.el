b569a62781126cba8b72616d105fe5a8fbd50ce2
Robb Enzmann <robbenzmann@gmail.com>
2022-11-04 23:22:30 -0400

    remove old config folder

--- config/linux.el
+++
@@ -1,7 +0,0 @@
-;; ============================================================================
-;;                              Linux
-;; ============================================================================
-(when (eq system-type 'gnu/linux)
-  (set-face-attribute 'default nil :font "Hack Nerd Font Mono-11")
-  ;; (exec-path-from-shell-initialize)
-  )
--- config/treesitter.el
+++
@@ -1,8 +0,0 @@
-;; ============================================================================
-;; 			      TreeSitter
-;; ============================================================================
-(unless (string= (getenv "HOSTNAME") "xhadrevrm7p.aetna.com")
-  (require 'tree-sitter)
-  (require 'tree-sitter-langs)
-  (global-tree-sitter-mode)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
