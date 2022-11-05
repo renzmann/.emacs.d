70ed41816c041db60e5af1067eb9c787e2b13b70
Robb Enzmann <robbenzmann@gmail.com>
2022-11-04 23:17:15 -0400

    add custom.el

---
+++ custom.el
@@ -0,0 +1,11 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(mode-line-in-non-selected-windows t)
+ '(org-agenda-files '("~/.emacs.d/org/work.org"))
+ '(org-hugo-front-matter-format "yaml")
+ '(package-selected-packages
+   '(use-package visual-fill-column ef-themes org-modern poly-markdown diff-hl ox-hugo tramp restart-emacs gnuplot corfu-terminal corfu esup consult org-roam ob-async sqlup-mode sql-indent ripgrep blacken sqlformat pythonic f s reformatter change-inner vterm magit vertico tree-sitter-langs tree-sitter orderless ob-sql-mode yaml-mode exec-path-from-shell vimrc-mode csv-mode haskell-mode julia-mode lua-mode go-mode scala-mode rust-mode markdown-mode eglot marginalia)))
