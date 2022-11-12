c3cee499d7ef030fadc41fc47244cc762d674d08
renzmann <32076780+renzmann@users.noreply.github.com>
2022-11-12 12:40:31 -0500

    add ahk-mode sources

---
+++ elpa/ahk-mode-20200412.1832/ahk-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ahk-mode.el  -*- no-byte-compile: t -*-
+(define-package "ahk-mode" "20200412.1832" "Major mode for editing AHK (AutoHotkey and AutoHotkey_L)" '((emacs "24.3")) :commit "729007b5f22a49f5187ff47fca18c0d674e73047" :authors '(("Rich Alesi")) :maintainer '("Rich Alesi") :keywords '("ahk" "autohotkey" "hotkey" "keyboard shortcut" "automation") :url "https://github.com/ralesi/ahk-mode")
