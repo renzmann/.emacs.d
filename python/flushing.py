de772025aea0fa43c4ecdd7259af438ade0a8868
renzmann <32076780+renzmann@users.noreply.github.com>
2022-11-29 22:52:40 -0500

    remove tree-sitter and tree-sitter langs for now - we'll retry soon
    Keep getting `tree sitter cannot find shared library for language: python` error

--- python/flushing.py
+++ python/flushing.py
@@ -17,6 +17,7 @@ args = parser.parse_args()
 def cycle(
     length: int = DEFAULT_LENGTH, sleep: float = DEFAULT_SLEEP, flush: bool = True
 ) -> None:
+    """Print `'Hello'` with a rotating number of `'!'` on the same line."""
     for i in range(1, length + 1):
         if i > 1:
             print("\033[K", end="")  # Clear to the end of line
@@ -27,6 +28,7 @@ def cycle(
 def straight(
     length: int = DEFAULT_LENGTH, sleep: float = DEFAULT_SLEEP, flush: bool = True
 ) -> None:
+    """Print `'Hello!'` a fixed number of times in a row, optionally flushing each time."""
     for _ in range(length):
         print("Hello!", flush=flush)
         time.sleep(sleep)
