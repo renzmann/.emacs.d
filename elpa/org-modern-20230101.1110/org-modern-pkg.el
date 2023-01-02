b1ec5c759bf462bc04564766ffce0fc32cbd096b
Robb Enzmann <robbenzmann@gmail.com>
2023-01-02 18:01:44 -0500

    update org-modern

---
+++ elpa/org-modern-20230101.1110/org-modern-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from org-modern.el  -*- no-byte-compile: t -*-
+(define-package "org-modern" "20230101.1110" "Modern looks for Org" '((emacs "27.1")) :commit "f7cf2bb4a763472c6b90cabab779b18325b40f5b" :authors '(("Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :url "https://github.com/minad/org-modern")
--- elpa/org-modern-20230101.1110/org-modern.el
+++ elpa/org-modern-20230101.1110/org-modern.el
@@ -1,13 +1,13 @@
 ;;; org-modern.el --- Modern looks for Org -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022  Free Software Foundation, Inc.
+;; Copyright (C) 2022-2023 Free Software Foundation, Inc.
 
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2022
-;; Version: 0.6
-;; Package-Version: 20221209.1941
-;; Package-Commit: 067f9319d67b1170d46a394e039537bb9cb413e3
+;; Version: 0.7
+;; Package-Version: 20230101.1110
+;; Package-Commit: f7cf2bb4a763472c6b90cabab779b18325b40f5b
 ;; Package-Requires: ((emacs "27.1"))
 ;; Homepage: https://github.com/minad/org-modern
 
@@ -159,9 +159,12 @@ all other blocks."
                         (string :tag "#+end_NAME replacement"))
                   (const :tag "Hide #+begin_ and #+end_ prefixes" t)))))
 
-(defcustom org-modern-block-fringe t
-  "Add a bitmap fringe to blocks."
-  :type 'boolean)
+(defcustom org-modern-block-fringe 0
+  "Add a border to the blocks in the fringe.
+This variable can also be set to an integer between 0 and 16,
+which specifies the offset of the block border from the edge of
+the window."
+  :type '(choice boolean integer))
 
 (defcustom org-modern-keyword t
   "Prettify keywords like #+title.
@@ -566,13 +569,16 @@ the font.")
              (fboundp 'fringe-bitmap-p)
              (not (fringe-bitmap-p 'org-modern--block-inner)))
     (let* ((g (ceiling (frame-char-height) 1.8))
-           (h (- (default-line-height) g)))
+           (h (- (default-line-height) g))
+           (v (expt 2 (- 15 (if (booleanp org-modern-block-fringe) 0
+                              org-modern-block-fringe))))
+           (w (+ v v -1)))
       (define-fringe-bitmap 'org-modern--block-inner
-        [128] nil nil '(top t))
+        (vector v) nil 16 '(top t))
       (define-fringe-bitmap 'org-modern--block-begin
-        (vconcat (make-vector g 0) [#xFF] (make-vector (- 127 g) #x80)) nil nil 'top)
+        (vconcat (make-vector g 0) (vector w) (make-vector (- 127 g) v)) nil 16 'top)
       (define-fringe-bitmap 'org-modern--block-end
-        (vconcat (make-vector (- 127 h) #x80) [#xFF] (make-vector h 0)) nil nil 'bottom))))
+        (vconcat (make-vector (- 127 h) v) (vector w) (make-vector h 0)) nil 16 'bottom))))
 
 (defun org-modern--symbol (str)
   "Add `org-modern-symbol' face to STR."
