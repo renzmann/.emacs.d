f2661f087796a8bb7f17e895aa0cbaed3752c921
Robb Enzmann <robbenzmann@gmail.com>
2023-01-02 18:01:54 -0500

    update rust-mode

--- elpa/rust-mode-20221225.2240/rust-mode-pkg.el
+++ elpa/rust-mode-20221225.2240/rust-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "rust-mode" "20220919.1051" "A major-mode for editing Rust source code"
+(define-package "rust-mode" "20221225.2240" "A major-mode for editing Rust source code"
   '((emacs "25.1"))
-  :commit "1d24ca2e6c18cd036f00e930cca64f44db04fb67" :authors
+  :commit "0431b10d2520918f3f250fdf4dc96e8d2eb7ea76" :authors
   '(("Mozilla"))
   :maintainer
   '("Mozilla")
