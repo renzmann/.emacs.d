85e6339eed282bdff2b394eabfe50830dcb7009e
Robb Enzmann <robbenzmann@gmail.com>
2023-01-02 18:01:34 -0500

    update orderless

--- elpa/orderless-20221225.1428/orderless-pkg.el
+++ elpa/orderless-20221225.1428/orderless-pkg.el
@@ -1,6 +1,6 @@
-(define-package "orderless" "20221128.2330" "Completion style for matching regexps in any order"
+(define-package "orderless" "20221225.1428" "Completion style for matching regexps in any order"
   '((emacs "26.1"))
-  :commit "004cee6b8e01f8eb0cb1c683d0a637b14890600f" :authors
+  :commit "847694e78c12d903d5e3f6cb365a5d3b984db537" :authors
   '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
   :maintainer
   '("Omar Antolín Camarena" . "omar@matem.unam.mx")
