e448545c11fce1c5629227858d25d908097445c5
Robb Enzmann <EnzmannR@aetna.com>
2023-01-04 15:21:25 -0500

    remove autocompletion source files

--- elpa/vertico-1.0/vertico-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from vertico.el  -*- no-byte-compile: t -*-
-(define-package "vertico" "1.0" "VERTical Interactive COmpletion" '((emacs "27.1")) :commit "af1c893f3891902162e32f73f062213436636567" :authors '(("Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :url "https://github.com/minad/vertico")
