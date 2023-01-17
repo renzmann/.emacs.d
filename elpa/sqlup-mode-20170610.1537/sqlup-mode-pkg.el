7b9e0c471770b1a58f47e64855675e482e1c4574
Robb Enzmann <EnzmannR@aetna.com>
2023-01-17 15:56:54 -0500

    remove SQL related extra dependencies

--- elpa/sqlup-mode-20170610.1537/sqlup-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from sqlup-mode.el  -*- no-byte-compile: t -*-
-(define-package "sqlup-mode" "20170610.1537" "Upcase SQL words for you" 'nil :commit "04970977b4abb4d44301651618bbf1cdb0b263dd" :authors '(("Aldric Giacomoni" . "trevoke@gmail.com")) :maintainer '("Aldric Giacomoni" . "trevoke@gmail.com") :keywords '("sql" "tools" "redis" "upcase") :url "https://github.com/trevoke/sqlup-mode.el")
