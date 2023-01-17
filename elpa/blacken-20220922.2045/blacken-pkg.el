5cde9ca066715eab0f17e3a2499c905b6276b7d1
Robb Enzmann <EnzmannR@aetna.com>
2023-01-17 15:59:26 -0500

    remove blacken package

--- elpa/blacken-20220922.2045/blacken-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from blacken.el  -*- no-byte-compile: t -*-
-(define-package "blacken" "20220922.2045" "Reformat python buffers using the \"black\" formatter" '((emacs "25.2")) :commit "456596e00f8277eafd9a08c62a71df06e8cad2c5" :authors '(("Artem Malyshev" . "proofit404@gmail.com")) :maintainer '("Artem Malyshev" . "proofit404@gmail.com") :url "https://github.com/proofit404/blacken")
