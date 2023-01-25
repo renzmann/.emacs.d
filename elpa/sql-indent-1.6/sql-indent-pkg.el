99ffeac0cae4cc899213922710c032b9648537e9
Robb Enzmann <robbenzmann@gmail.com>
2023-01-25 10:11:09 -0500

    add sql-indent sources back in

---
+++ elpa/sql-indent-1.6/sql-indent-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from sql-indent.el  -*- no-byte-compile: t -*-
+(define-package "sql-indent" "1.6" "Support for indenting code in SQL files." '((cl-lib "0.5")) :keywords '("languages" "sql") :authors '(("Alex Harsanyi" . "AlexHarsanyi@gmail.com")) :maintainer '("Alex Harsanyi" . "AlexHarsanyi@gmail.com") :url "https://github.com/alex-hhh/emacs-sql-indent")
