a05a942be96eb1528dd25ffd49181d58421d9c40
renzmann <32076780+renzmann@users.noreply.github.com>
2023-01-27 10:37:44 -0500

    update packages

--- elpa/marginalia-20230101.1109/marginalia-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from marginalia.el  -*- no-byte-compile: t -*-
-(define-package "marginalia" "20230101.1109" "Enrich existing commands with completion annotations" '((emacs "27.1")) :commit "077fcfb548eaa076c08431b0d712ea947d42c66c" :authors '(("Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler" . "mail@daniel-mendler.de") :url "https://github.com/minad/marginalia")
