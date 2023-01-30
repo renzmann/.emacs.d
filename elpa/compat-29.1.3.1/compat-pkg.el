7cc7dba9e9efd0549006c91bf786ccee42afc201
Robb Enzmann <robbenzmann@gmail.com>
2023-01-30 10:26:04 -0500

    add compat dependency

---
+++ elpa/compat-29.1.3.1/compat-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from compat.el  -*- no-byte-compile: t -*-
+(define-package "compat" "29.1.3.1" "Emacs Lisp Compatibility Library" '((emacs "24.4")) :commit "27961369b39288454559289f3ceecdf377241cbc" :authors '(("Philip Kaludercic <philipk@posteo.net>, Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Daniel Mendler <mail@daniel-mendler.de>, Compat Development" . "~pkal/compat-devel@lists.sr.ht") :keywords '("lisp") :url "https://github.com/emacs-compat/compat")
