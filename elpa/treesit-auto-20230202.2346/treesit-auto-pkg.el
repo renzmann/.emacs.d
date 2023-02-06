62a9f50053d86a2f50c07ff0c867f8cf0b0179f9
Robb Enzmann <EnzmannR@aetna.com>
2023-02-06 08:21:50 -0500

    add melpa version of treesit-auto

---
+++ elpa/treesit-auto-20230202.2346/treesit-auto-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from treesit-auto.el  -*- no-byte-compile: t -*-
+(define-package "treesit-auto" "20230202.2346" "Automatically use tree-sitter enhanced major modes when available" '((emacs "29.0")) :commit "cdf2984e3739401b632d99c5d35e35da78cac090" :authors '(("Robb Enzmann" . "robbenzmann@gmail.com")) :maintainer '("Robb Enzmann" . "robbenzmann@gmail.com") :keywords '("treesitter" "auto" "automatic" "major" "mode" "fallback" "convenience") :url "https://github.com/renzmann/treesit-auto.git")
