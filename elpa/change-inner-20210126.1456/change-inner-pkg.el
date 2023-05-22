603998672b4ae194d0a0e301be25f1ebc3454de7
renzmann <32076780+renzmann@users.noreply.github.com>
2023-05-22 07:06:41 -0400

    Remove change-inner

--- elpa/change-inner-20210126.1456/change-inner-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from change-inner.el  -*- no-byte-compile: t -*-
-(define-package "change-inner" "20210126.1456" "Change contents based on semantic units" '((expand-region "0.7")) :commit "42cad58aed2caec260f8e8ff61f78a7d3db72d1b" :authors '(("Magnar Sveen" . "magnars@gmail.com")) :maintainer '("Magnar Sveen" . "magnars@gmail.com") :keywords '("convenience" "extensions"))
