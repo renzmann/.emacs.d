87d5b7ad876fe4728e033281a1456386a4b80a9c
Robb Enzmann <robbenzmann@gmail.com>
2023-08-02 21:23:25 -0400

    remove most font config

--- font.el
+++ font.el
@@ -1,11 +1,3 @@
-(set-face-attribute 'default nil :font "Hack Nerd Font Mono-11")
-
 (when (renz/windowsp)
     ;; Set a font that supports emoji
-    (set-fontset-font t 'unicode (font-spec :family "Segoe UI Emoji") nil 'prepend)
-    (set-face-attribute 'default nil :font "Hack NF-12"))
-
-(when (eq system-type 'darwin)
-  ;; Uncomment this if we can't install Hack Nerd font
-  ;; (set-face-attribute 'default nil :font "Menlo-14")
-  (set-face-attribute 'default nil :font "Hack Nerd Font Mono-13"))
+    (set-fontset-font t 'unicode (font-spec :family "Segoe UI Emoji") nil 'prepend))
