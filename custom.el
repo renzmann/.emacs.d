f26796ac9417cf3333a1645c2b001c20405f7e95
Robb Enzmann <robbenzmann@gmail.com>
2023-10-09 05:46:04 -0400

    Add protobuf mode to custom

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  '(mode-line-in-non-selected-windows t)
  '(org-hugo-front-matter-format "yaml")
  '(package-selected-packages
-   '(ob-powershell powershell ob-async code-cells blacken coterm julia-mode pyvenv ess sql-indent package-lint vimrc-mdoe ox-clip org visual-fill-column ef-themes org-modern ox-hugo esup orderless yaml-mode exec-path-from-shell vimrc-mode csv-mode haskell-mode lua-mode scala-mode markdown-mode))
+   '(protobuf-ts-mode ob-powershell powershell ob-async code-cells blacken coterm julia-mode pyvenv ess sql-indent package-lint vimrc-mdoe ox-clip org visual-fill-column ef-themes org-modern ox-hugo esup orderless yaml-mode exec-path-from-shell vimrc-mode csv-mode haskell-mode lua-mode scala-mode markdown-mode))
  '(safe-local-variable-values
    '((vc-prepare-patches-separately)
      (diff-add-log-use-relative-names . t)
