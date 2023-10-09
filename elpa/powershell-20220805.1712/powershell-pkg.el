c2884850b602d820f0514bbe407a650822ccfdec
Robb Enzmann <robbenzmann@gmail.com>
2023-10-09 05:11:37 -0400

    Add powershell sources

---
+++ elpa/powershell-20220805.1712/powershell-pkg.el
@@ -0,0 +1,14 @@
+(define-package "powershell" "20220805.1712" "Mode for editing PowerShell scripts"
+  '((emacs "24"))
+  :commit "f2da15857e430206e215a3c65289b4058ae3c976" :authors
+  '(("Frédéric Perrin <frederic (dot) perrin (arobas) resel (dot) fr>"))
+  :maintainers
+  '(("Frédéric Perrin <frederic (dot) perrin (arobas) resel (dot) fr>"))
+  :maintainer
+  '("Frédéric Perrin <frederic (dot) perrin (arobas) resel (dot) fr>")
+  :keywords
+  '("powershell" "languages")
+  :url "http://github.com/jschaf/powershell.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
