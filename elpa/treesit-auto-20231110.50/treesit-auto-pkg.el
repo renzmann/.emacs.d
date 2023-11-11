c696dc87224a480b55de31fdcf3b5280eee242d0
Robb Enzmann <robbenzmann@gmail.com>
2023-11-10 21:02:36 -0500

    Add latest treesit-auto sources

---
+++ elpa/treesit-auto-20231110.50/treesit-auto-pkg.el
@@ -0,0 +1,14 @@
+(define-package "treesit-auto" "20231110.50" "Automatically use tree-sitter enhanced major modes"
+  '((emacs "29.0"))
+  :commit "090a74deeff29530dbc0837ec3151dac8aa497e7" :authors
+  '(("Robb Enzmann" . "robbenzmann@gmail.com"))
+  :maintainers
+  '(("Robb Enzmann" . "robbenzmann@gmail.com"))
+  :maintainer
+  '("Robb Enzmann" . "robbenzmann@gmail.com")
+  :keywords
+  '("treesitter" "auto" "automatic" "major" "mode" "fallback" "convenience")
+  :url "https://github.com/renzmann/treesit-auto.git")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
