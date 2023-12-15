bd8127f73ed73253c978785277be38d361fab0e4
Robb Enzmann <EnzmannR@aetna.com>
2023-12-15 07:48:47 -0500

    Add corfu sources

---
+++ elpa/treesit-auto-20231130.1716/treesit-auto-pkg.el
@@ -0,0 +1,14 @@
+(define-package "treesit-auto" "20231130.1716" "Automatically use tree-sitter enhanced major modes"
+  '((emacs "29.0"))
+  :commit "cfde372ca1be3c216e3b52dd7b58a7957b67e25f" :authors
+  '(("Robb Enzmann" . "robbenzmann@gmail.com"))
+  :maintainers
+  '(("Robb Enzmann" . "robbenzmann@gmail.com"))
+  :maintainer
+  '("Robb Enzmann" . "robbenzmann@gmail.com")
+  :keywords
+  '("treesitter" "auto" "automatic" "major" "mode" "fallback" "convenience")
+  :url "https://github.com/renzmann/treesit-auto.git")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
