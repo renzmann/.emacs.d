8584636c974940b30ce2874cf654ab25e8ddbd13
Robb Enzmann <EnzmannR@aetna.com>
2023-12-29 09:08:27 -0500

    Add caddyfile-mode sources

---
+++ elpa/caddyfile-mode-20220626.945/caddyfile-mode-pkg.el
@@ -0,0 +1,15 @@
+(define-package "caddyfile-mode" "20220626.945" "Major mode for Caddy configuration files"
+  '((emacs "25")
+    (loop "1.3"))
+  :commit "fc41148f5a7eb320f070666f046fb9d88cf17680" :authors
+  '(("Thomas Jost" . "schnouki@schnouki.net"))
+  :maintainers
+  '(("Thomas Jost" . "schnouki@schnouki.net"))
+  :maintainer
+  '("Thomas Jost" . "schnouki@schnouki.net")
+  :keywords
+  '("languages")
+  :url "https://github.com/Schnouki/caddyfile-mode/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/loop-20160813.1407/loop-pkg.el
@@ -0,0 +1,11 @@
+(define-package "loop" "20160813.1407" "friendly imperative loop structures" 'nil :commit "0ce77271d56b0fcdba4b3b38fed526081cd1f674" :authors
+  '(("Wilfred Hughes" . "me@wilfred.me.uk"))
+  :maintainers
+  '(("Wilfred Hughes" . "me@wilfred.me.uk"))
+  :maintainer
+  '("Wilfred Hughes" . "me@wilfred.me.uk")
+  :keywords
+  '("loop" "while" "for each" "break" "continue"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
