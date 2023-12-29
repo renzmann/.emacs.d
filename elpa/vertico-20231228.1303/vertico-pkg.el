61bca56bd0fa2a42869ba08b2ba7a9a89c078bf1
Robb Enzmann <EnzmannR@aetna.com>
2023-12-29 09:04:57 -0500

    Add vertico sources

---
+++ elpa/vertico-20231228.1303/vertico-pkg.el
@@ -0,0 +1,15 @@
+(define-package "vertico" "20231228.1303" "VERTical Interactive COmpletion"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "37e01c10bf38fd154d90ec315aaf6414a046879d" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("convenience" "files" "matching" "completion")
+  :url "https://github.com/minad/vertico")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
