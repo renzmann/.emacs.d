72de9976af9d50c4429b88383f35e23c7596a3c2
Robb Enzmann <robbenzmann@gmail.com>
2024-03-29 15:57:28 -0400

    Add consult sources (giving it a try for a bit)

---
+++ elpa/consult-20240323.2016/consult-pkg.el
@@ -0,0 +1,15 @@
+(define-package "consult" "20240323.2016" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "ec232fa60497e7a3abcf6e385181afcc0cf6017f" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("matching" "files" "completion")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
