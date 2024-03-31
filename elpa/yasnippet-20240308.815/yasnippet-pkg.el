5821c83e632ec8eb80aa48057aeb4245e3c1732b
Robb Enzmann <robbenzmann@gmail.com>
2024-03-31 10:28:42 -0400

    Add yasnippet sources

---
+++ elpa/yasnippet-20240308.815/yasnippet-pkg.el
@@ -0,0 +1,13 @@
+(define-package "yasnippet" "20240308.815" "Yet another snippet extension for Emacs"
+  '((cl-lib "0.5")
+    (emacs "24.4"))
+  :commit "33587a8551b8f6991b607d3532062a384c010ce1" :maintainers
+  '(("Noam Postavsky" . "npostavs@gmail.com"))
+  :maintainer
+  '("Noam Postavsky" . "npostavs@gmail.com")
+  :keywords
+  '("convenience" "emulation")
+  :url "http://github.com/joaotavora/yasnippet")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
