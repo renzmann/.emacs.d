123c37a06921eaf2611770f45c838a5de6206f46
Robb Enzmann <robbenzmann@gmail.com>
2025-12-18 15:43:43 -0500

    Fix custom.el missing closing bracket

--- custom.el
+++ custom.el
@@ -11,7 +11,7 @@
              dockerfile-mode ellama exec-path-from-shell go-mode
              groovy-mode jq-mode marginalia markdown-mode orderless
              package-lint powershell sql-indent typescript-mode vertico
-             vterm yaml-mode yasnippet)
+             vterm yaml-mode yasnippet))
  '(package-vc-selected-packages
    '((treesit-auto :vc-backend Git :url "https://github.com/renzmann/treesit-auto")))
  '(safe-local-variable-values
